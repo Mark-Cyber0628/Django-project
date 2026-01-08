950374b1b49ffe1739b1ab01892b350583d2ff24
Mark-Cyber0628 <szakalmark0628@gmail.com>
2026-01-08 05:51:18 -0500

    new models and admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,43 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+# <JAVASLAT> Importáld az új modelleket itt
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# <JAVASLAT> Regisztráld a QuestionInline és ChoiceInline osztályokat itt
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
-# Register your models here.
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+# Regisztráld a modelleket itt.
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
+# <JAVASLAT> Regisztráld a Question és Choice modelleket itt
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
